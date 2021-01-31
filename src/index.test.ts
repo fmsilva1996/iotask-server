8c2cc4d74471b18e182662ec04257da2925c5c7a
Francisco Silva <fran.silva@thebeansgroup.com>
2021-01-31 13:36:26 +0000

    Setup Express-GraphQL
    Add basic server config and graphql schema and resolvers

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { isFoo } from './index'
-
-test('should return true ONLY if given foo', () => {
-  expect(isFoo('foo')).toBe(true)
-  expect(isFoo('bar')).toBe(false)
-  expect(isFoo('baz')).toBe(false)
-})
